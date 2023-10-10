6535b5f172bbb924e984022f0c009fd60962b6d7
Dexter Manlangit <officialrawkxterr@yahoo.com>
2023-10-11 02:34:22 +0800

    - Added properties to BubbleSort and QuickSort Model - Formatting improvements

--- Model/BubbleSortModel.cs
+++ Model/BubbleSortModel.cs
@@ -3,18 +3,20 @@ namespace WindowsFormsApp1.Model
 {
     public class BubbleSortModel
     {
-        private IBubbleSort _bubbleSort;
 
+        private IBubbleSort _bubbleSort;
+        /* -- PROPS SECTION = use this if you want to access this from other parts of your code -- */
+        private char[] Data { get; set; } 
         public BubbleSortModel(IBubbleSort bubbleSort)
         {
             _bubbleSort = bubbleSort;
         }
-
        
-
         public void BubbleSortStrings(char[] data)
         {
-            _bubbleSort.BubbleSortIt(data);
+             Data = data;
+            _bubbleSort.BubbleSortIt(Data);
         }
+
     }
 }
--- Model/QuickSortModel.cs
+++ Model/QuickSortModel.cs
@@ -1,11 +1,14 @@
 ﻿using WindowsFormsApp1.SortService.QuickSort;
-
 namespace WindowsFormsApp1.Model
 {
     public class QuickSortModel
     {
 
         private IQuickSort _quickSort;
+        /* -- PROPS SECTION = use this if you want to access this from other parts of your code -- */
+        private char[] Data { get; set; } 
+        private int Left { get; set; }
+        private int Right { get; set; }
 
         public QuickSortModel(IQuickSort quickSort)
         {
@@ -15,7 +18,9 @@ namespace WindowsFormsApp1.Model
 
         public void QuickSortStrings(char[] data, int left, int right)
         {
-            _quickSort.QuickSortIt(data, left, right);
+            Data = data; Left = left; Right = right;
+            _quickSort.QuickSortIt(Data, Left, Right);
         }
+
     }
 }
